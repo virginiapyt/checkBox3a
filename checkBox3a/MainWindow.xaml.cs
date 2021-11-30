9387f2f9a411548cf0a46b926e0a4455b0c1f7e9
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2021-11-30 10:30:33 +0100

    Dodaj pliki projektów.

---
+++ checkBox3a/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace checkBox3a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void check_master_Click(object sender, RoutedEventArgs e)
+        {
+            check1.IsChecked = check2.IsChecked = check3.IsChecked = 
+                (sender as CheckBox).IsChecked;
+        }
+    }
+}
